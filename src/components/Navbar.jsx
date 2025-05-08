818b971ea131efd1af9347b246985ed35d23ac01
Deepu Kunjumon <deepukunjumon595@gmail.com>
2025-05-08 21:07:06 +0530

    Fixed layout alignments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,88 +1,88 @@
 import React, { useState } from "react";
 import {
-    AppBar,
-    Toolbar,
-    Typography,
-    IconButton,
-    Menu,
-    MenuItem,
-    Avatar,
-    Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  Avatar,
+  Box,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../constants/routes";
 
 const Navbar = ({ toggleDrawer }) => {
-    const navigate = useNavigate();
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
-    const handleAvatarClick = (event) => setAnchorEl(event.currentTarget);
-    const handleClose = () => setAnchorEl(null);
+  const handleAvatarClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
-    const handleLogout = () => {
-        localStorage.clear();
-        handleClose();
-        navigate(ROUTES.LOGIN);
-    };
+  const handleLogout = () => {
+    localStorage.clear();
+    handleClose();
+    navigate(ROUTES.LOGIN);
+  };
 
-    const handleProfile = () => {
-        handleClose();
-        navigate(ROUTES.PROFILE);
-    };
+  const handleProfile = () => {
+    handleClose();
+    navigate(ROUTES.PROFILE);
+  };
 
-    return (
-        <AppBar
-            position="fixed"
-            sx={{
-                zIndex: (theme) => theme.zIndex.drawer + 1,
-                width: "100%",
-            }}
-        >
-            <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-                <Box display="flex" alignItems="center">
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        onClick={toggleDrawer}
-                        sx={{ mr: 1 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div">
-                        Dashboard
-                    </Typography>
-                </Box>
+  return (
+    <AppBar
+      position="fixed"
+      sx={{
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        width: "100%",
+      }}
+    >
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box display="flex" alignItems="center">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer}
+            sx={{ mr: 1 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div">
+            TBMS
+          </Typography>
+        </Box>
 
-                <Box>
-                    <IconButton onClick={handleAvatarClick} color="inherit">
-                        <Avatar
-                            sx={{
-                                bgcolor: "secondary.main",
-                                width: { xs: 32, sm: 36, md: 40 },
-                                height: { xs: 32, sm: 36, md: 40 },
-                                fontSize: { xs: 14, sm: 16, md: 18 },
-                            }}
-                        >
-                            U
-                        </Avatar>
-                    </IconButton>
-                    <Menu
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-                        transformOrigin={{ vertical: "top", horizontal: "right" }}
-                    >
-                        <MenuItem onClick={handleProfile}>Profile</MenuItem>
-                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                    </Menu>
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
+        <Box>
+          <IconButton onClick={handleAvatarClick} color="inherit">
+            <Avatar
+              sx={{
+                bgcolor: "secondary.main",
+                width: { xs: 32, sm: 36, md: 40 },
+                height: { xs: 32, sm: 36, md: 40 },
+                fontSize: { xs: 14, sm: 16, md: 18 },
+              }}
+            >
+              U
+            </Avatar>
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            transformOrigin={{ vertical: "top", horizontal: "right" }}
+          >
+            <MenuItem onClick={handleProfile}>Profile</MenuItem>
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
