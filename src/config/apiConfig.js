a946a7ffd986d46c38e2aad0335a8f0737ec6325
Deepu Kunjumon <deepu.k@acumengroup.in>
2025-05-20 11:36:04 +0530

    Test railway connection

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,7 @@
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8000/api"
-    : "https://52.204.98.211/api";
+    : "https://api-tbms.up.railway.app/api";
 
 const apiConfig = {
   BASE_URL: API_BASE_URL,
