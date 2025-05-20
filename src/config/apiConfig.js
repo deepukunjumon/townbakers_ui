66898b94159d6134837d6962a83d586b1d147627
Deepu Kunjumon <deepu.k@acumengroup.in>
2025-05-20 11:48:45 +0530

    Test 2

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,7 @@
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8000/api"
-    : "https://tbms_ui.railway.internal";
+    : "https://town_bakers.railway.internal";
 
 const apiConfig = {
   BASE_URL: API_BASE_URL,
