a1bd477d14205f2e9806a65714bb774f2a191c50
Deepu Kunjumon <deepu.k@acumengroup.in>
2025-06-13 13:44:27 +0530

    Added analytics from vercel

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { React, useEffect } from "react";
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <BrowserRouter>
       <CssBaseline />
       <AppRoutes />
+      <Analytics />
     </BrowserRouter>
   );
 }
