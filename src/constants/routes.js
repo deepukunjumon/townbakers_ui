9fc02571a36f50ebbc7301d1a1d79b143ced64e0
Deepu Kunjumon <deepu.k@acumengroup.in>
2025-06-30 17:21:43 +0530

    reset password fix

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,7 @@
 export const ROUTES = {
   ROOT: "/",
   LOGIN: "/login",
+  RESET_PASSWORD: "/password/reset",
   DEFAULT_PASSWORD_RESET: "/default-password/reset",
   UNAUTHORIZED: "/unauthorized",
   PROFILE: "/profile",
